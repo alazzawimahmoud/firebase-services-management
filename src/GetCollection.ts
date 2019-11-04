e30678c3485209756eee0ee8faa80796b259d36a
Mahmoud.AlAzzawi@caresolutions.be <alazzawi.mahmoud@gmail.com>
2019-11-04 16:48:03 +0100

    Minor

--- src/GetCollection.ts
+++ src/GetCollection.ts
@@ -39,7 +39,8 @@ function getQueryBatch(
 
     query.get()
         .then((snapshot) => {
-
+            console.log(`[getQueryBatch] snapshot.size`, snapshot.size);
+            
             // When there are no documents left, we are done
             if (snapshot.size == 0) {
                 return 0;
