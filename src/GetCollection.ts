80bf7c771f6cceeaf47b5f71cd1f3570bd48bc86
Mahmoud.AlAzzawi@caresolutions.be <alazzawi.mahmoud@gmail.com>
2019-11-05 11:24:09 +0100

    Adding cursor to getCollection

--- src/GetCollection.ts
+++ src/GetCollection.ts
@@ -6,29 +6,42 @@ interface IGetCollectionProps {
     snapshotCallback: snapshotCallbackType
     collectionPath?: string,
     batchSize?: number,
+    orderByKey?: string,
     query?: FirebaseFirestore.Query,
 }
-export default function getCollection({ db, collectionPath, batchSize = 10, query, snapshotCallback }: IGetCollectionProps) {
+
+export default function getCollection({ db, collectionPath, batchSize = 10, orderByKey = 'timestamp', query, snapshotCallback }: IGetCollectionProps) {
     if (!db) throw "[getCollection] No db provided";
     if (!query && !collectionPath) throw "[getCollection] No query OR collectionPath provided";
 
     let _query: FirebaseFirestore.Query;
+
     if (query) {
         _query = query;
     } else if (!query && collectionPath) {
-        _query = db.collection(collectionPath).orderBy('timestamp');
+        _query = db.collection(collectionPath);
     }
 
     return new Promise((resolve, reject) => {
-        getQueryBatch(_query.limit(batchSize), batchSize, resolve, reject, snapshotCallback);
+        getQueryBatch(
+            db,
+            _query.orderBy(orderByKey).limit(batchSize),
+            batchSize,
+            orderByKey,
+            resolve,
+            reject,
+            snapshotCallback);
     });
 }
 
 let loadCounter = 0;
+let lastCursor: FirebaseFirestore.QueryDocumentSnapshot;
 
 function getQueryBatch(
+    db: FirebaseFirestore.Firestore,
     query: FirebaseFirestore.Query,
     batchSize: number,
+    orderByKey: string,
     resolve: (value?: {} | PromiseLike<{}> | undefined) => void,
     reject: (reason?: any) => void,
     snapshotCallback: snapshotCallbackType
@@ -36,15 +49,20 @@ function getQueryBatch(
 
     console.log(`[getQueryBatch] started `)
 
+
+
     query.get()
         .then((snapshot) => {
-            
+
             console.log(`[getQueryBatch] snapshot.size`, snapshot.size);
 
             // When there are no documents left, we are done
-            if (snapshot.size == 0) {
+            if (snapshot.size === 0) {
                 return 0;
             }
+
+            // Get the last document
+            lastCursor = snapshot.docs[snapshot.docs.length - 1];
 
             return snapshotCallback(snapshot)
                 .then(() => {
@@ -57,11 +75,20 @@ function getQueryBatch(
                 resolve(loadCounter);
                 return;
             }
+
+            // Construct a new query starting at this document.
+            // Note: this will not have the desired effect if multiple
+            // cities have the exact same population value.
+            let nextQuery = query
+                .orderBy(orderByKey)
+                .startAfter(lastCursor.data()[orderByKey])
+                .limit(batchSize);
+
             console.log(`[getQueryBatch] ${loadCounter} loaded`)
             // Recurse on the next process tick, to avoid
             // exploding the stack.
             process.nextTick(() => {
-                getQueryBatch(query.limit(batchSize), batchSize, resolve, reject, snapshotCallback);
+                getQueryBatch(db, nextQuery, batchSize, orderByKey, resolve, reject, snapshotCallback);
             });
         })
         .catch(reject);
